d17645f1c7c6920b1821369ca99f7344d2de923a
Abbas Ghaniwala <abbasghaniwala92@gmail.com>
2022-09-19 16:54:08 +0530

    Create cylinder.java

---
+++ q3/src/main/java/cylinder.java
@@ -0,0 +1,16 @@
+import org.springframework.stereotype.Component;
+
+@Component
+public class cylinder implements shape {
+
+	
+	void start() {
+		System.out.println("cylinder generated");
+	}
+
+	public void so() {
+		// TODO Auto-generated method stub
+		start();
+		
+	}
+}
