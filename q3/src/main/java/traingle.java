e1fe4a5e2ec59bbb8060f33dd3adf0360cf5475b
Abbas Ghaniwala <abbasghaniwala92@gmail.com>
2022-09-19 16:52:01 +0530

    Create traingle.java

---
+++ q3/src/main/java/traingle.java
@@ -0,0 +1,16 @@
+import org.springframework.stereotype.Component;
+
+@Component
+public class triangle implements shape {
+
+	
+	void start() {
+		System.out.println("triangle generated");
+	}
+
+	public void go() {
+		// TODO Auto-generated method stub
+		start();
+		
+	}
+}
