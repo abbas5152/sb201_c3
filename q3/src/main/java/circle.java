ab691f1e3426c5e4290b275cc33029053178eda4
Abbas Ghaniwala <abbasghaniwala92@gmail.com>
2022-09-19 16:53:40 +0530

    Create circle.java

---
+++ q3/src/main/java/circle.java
@@ -0,0 +1,16 @@
+import org.springframework.stereotype.Component;
+
+@Component
+public class circle implements shape {
+
+	
+	void start() {
+		System.out.println("cirlce generated");
+	}
+
+	public void so() {
+		// TODO Auto-generated method stub
+		start();
+		
+	}
+}
